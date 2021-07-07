c165a2b19ec760310a9a7e1269759af854d54896
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-07-06 19:35:12 -0700

    commit tree 7336afae65aef01d9601fe0b64bfe22277cd35e8

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isOn, setOn] = useState(false)
+  
+  function handleClick() {
+    setOn((isOn) => (!isOn))
+  }
+
+  const appClass = isOn ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className= {appClass} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add"
+      className = {appClass}
+      onClick = {handleClick}
+      >Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
+// import Filter from "./Filter";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState("All")
+
+  function handleChange(event) {
+    setCategory(event.target.value)
+  }
+
+  const display = items.filter((item) => {
+    if (selectedCategory === "All") return true; 
+
+    return item.category === selectedCategory;
+  }); 
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      {/* <Filter onCategoryChange = {handleChange} /> */}
+    <div className="Filter">
+			<select name="filter" onChange = {handleChange} >
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+			</select>
+		</div>
       <ul className="Items">
-        {items.map((item) => (
+        {display.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
